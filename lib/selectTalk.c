c8700511af581077b7d716475580a5cb670d9984
seo <tjtjsejr204@naver.com>
2017-12-01 16:13:59 +0900

    modify selectTalk

--- lib/selectTalk.c
+++ lib/selectTalk.c
@@ -1,14 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-char *seltalk()
+int seltalk()
 {
    char *str = malloc(sizeof(char)*100);
 
    printf("분석하고싶은 대화파일을 입력해주세요 : ");
    scanf("%s", str);
 
-   return str;
+   divDate(str);
+
+   return 0;
 }
 
 int main()
