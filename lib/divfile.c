5d2581e2539656a123953b86715a5ef510292bd3
seo <tjtjsejr204@naver.com>
2017-12-06 00:24:43 +0900

    modified divfile.c

--- lib/divfile.c
+++ lib/divfile.c
@@ -5,20 +5,23 @@
 
 int divfile()//(FILE *fd)
 {
+   FILE *fd = fopen("test.txt", "r"); //test용. 나중에 삭제
    FILE *talkf;
 
    char year[3][10] = {"2015년", "2016년", "2017년"};
-   char str[];
+   char str[] = ".txt";
    char buf[1024];
-   char rd;
-//   char str15[] = "\n\n2015년 ";
-//   char str16[] = "\n\n2016년 ";
-//   char str17[] = "\n\n2017년 ";
+   char *rd;
+
+   if(fd == NULL){ //나중에 삭제
+	printf("원본파일 열기 실패\n");
+	return 1;
+   }
 
    for(int i=0; i<3; i++){
         //새 파일에 원본 내용 복사하기
         while(1){
-           rd = fgets(buf, sizeof(buf), fd);
+           rd = fgets(buf, sizeof(buf)+1, fd);
            if(feof(fd)!=0){ //원본파일 끝까지 읽으면
                 printf("대화의 마지막입니다.\n");
                 break;
@@ -26,13 +29,16 @@ int divfile()//(FILE *fd)
            //한문장씩 읽다가 파싱할 년도가 있으면
            else if(strstr(buf,year[i]) != 0){
                 //새 파일 생성
+		strcat(year[i], str);
                 talkf = fopen(year[i], "wt");
                 if(talkf == NULL){
                    printf("파일 생성 실패\n");
                    return 1;
                 }
                 fputs(rd, talkf);
-   }
+	   }
+	}
+   }fclose(fd);
 }
 
 
