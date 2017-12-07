6e7d859a89a41f054cb80ee76ca8b29177009d54
seo <tjtjsejr204@naver.com>
2017-12-07 23:58:10 +0900

    modified divfile.c

--- lib/divfile.c
+++ lib/divfile.c
@@ -18,26 +18,25 @@ int divfile()//(FILE *fd)
 	return 1;
    }
 
-   for(int i=0; i<3; i++){
-        //새 파일에 원본 내용 복사하기
-        while(1){
-           rd = fgets(buf, sizeof(buf)+1, fd);
-           if(feof(fd)!=0){ //원본파일 끝까지 읽으면
-                printf("대화의 마지막입니다.\n");
-                break;
+   //새 파일에 원본 내용 복사하기
+   fgets(buf, sizeof(buf)+1, fd);
+   for(int i=0; i<2; i++){
+	if(strstr(buf,year[i]) != 0){
+           //새 파일 생성
+	   strcat(year[i], str);
+           talkf = fopen(year[i], "wt");
+           if(talkf == NULL){
+                printf("파일 생성 실패\n");
+                return 1;
            }
-           //한문장씩 읽다가 파싱할 년도가 있으면
-           else if(strstr(buf,year[i]) != 0){
-                //새 파일 생성
-		strcat(year[i], str);
-                talkf = fopen(year[i], "wt");
-                if(talkf == NULL){
-                   printf("파일 생성 실패\n");
-                   return 1;
+           while(1){
+                fputs(buf, talkf);
+                fgets(buf, sizeof(buf)+1, fd);
+                if(strstr(buf, year[i+1]) != 0){
+                        break;
                 }
-                fputs(rd, talkf);
-	   }
-	}
+           }fclose(talkf);
+        }
    }fclose(fd);
 }
 
