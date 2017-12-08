d14d2e06aac7e3523b99b143d4ad995f3533ea24
seo <tjtjsejr204@naver.com>
2017-12-08 17:14:26 +0900

    divfile

--- lib/divfile.c
+++ lib/divfile.c
@@ -1,21 +1,13 @@
 #include <stdio.h>
 #include <string.h>
 
-//It is still incomplete!!!
-
-int divfile()//(FILE *fd)
+int divfile(FILE *fd)
 {
-   FILE *fd = fopen("test.txt", "r"); //test용. 나중에 삭제
    FILE *talkf;
 
-   char year[3][12] = {"2015년", "2016년", "2017년"};
+   char year[3][15] = {"2015년", "2016년", "2017년"};
    char str[] = ".txt";
    char buf[1024];
-
-   if(fd == NULL){ //나중에 삭제
-	printf("원본파일 열기 실패\n");
-	return 1;
-   }
 
    //새 파일에 원본 내용 복사하기
    fgets(buf, sizeof(buf), fd);
@@ -37,10 +29,4 @@ int divfile()//(FILE *fd)
            }fclose(talkf);
         }
    }fclose(fd);
-}
-
-
-int main ()
-{
-   divfile();
 }
