1dc1adb2a8a144b54a4721aae3d36a8b5156cc32
Chaeeun817 <chaeeun908@gmail.com>
2017-12-09 17:54:08 +0900

    wordcnt 년도 추가

--- lib/wordcnt.c
+++ lib/wordcnt.c
@@ -31,28 +31,52 @@ int main(void)
 	int hate1,hate2,hate3;
 	int countl=0;
 	int counth=0;
-	like1 = my_system("grep -c 사랑해 test.txt");
-	like2 = my_system("grep -c 보고싶어 test.txt");
-	like3 = my_system("grep -c 좋아해 test.txt");
 
-	countl = like1+like2+like3;
-
-	hate1 = my_system("grep -c 시발 test.txt");
-	hate2 = my_system("grep -c 존나 test.txt");
-	hate2 = my_system("grep -c 빡쳐 test.txt");
-
-	counth = hate1+hate2+hate3;
-
-	printf("%d\n", countl);
-	printf("%d\n", counth);
-	if(countl > counth)
+	if(str == 2015년)
 	{
-		printf("썸\n");
+		like1 = my_system("grep -c 사랑해 2015년.txt");
+        	like2 = my_system("grep -c 좋아해 2015년.txt");
+        	like3 = my_system("grep -c 보고싶어 2015년.txt");
+
+		hate1 = my_system("grep -c 시발 2015년.txt");
+		hate2 = my_system("grep -c 존나 2015년.txt");
+		hate2 = my_system("grep -c 빡쳐 2015년.txt");
+
+		counth = hate1+hate2+hate3;
+
+		if(countl > counth)
+		{
+			printf("썸\n");
+		}
+		else
+		{
+			printf("친구\n");
+		}
+	}
+	else if(str == 2016년)
+        {
+                like1 = my_system("grep -c 사랑해 2016년.txt");
+                like2 = my_system("grep -c 좋아해 2016년.txt");
+                like3 = my_system("grep -c 보고싶어 2016년.txt");
+
+                hate1 = my_system("grep -c 시발 2016년.txt");
+                hate2 = my_system("grep -c 존나 2016년.txt");
+                hate2 = my_system("grep -c 빡쳐 2016년.txt");
+
+                counth = hate1+hate2+hate3;
+
+                if(countl > counth)
+                {
+                        printf("썸\n");
+		}
+		else
+			printf("친구\n");
+
 	}
 	else
 	{
-		printf("친구\n");
+		printf("선택한 파일이 존재하지 않습니다.\n");
 	}
-
 	return 0;
 }
+
